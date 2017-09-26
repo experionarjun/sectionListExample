1b11eccd8d24b77f8e9e770cd44246c46fd0dd92
Arjun Sreedhar <arjun.sreedhar@experionglobal.com>
2017-09-26 10:13:35 +0530

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# SectionList Demo
+  Demo applicaton to demonstrate how SectionList works.
+
+# Usage
+
+Install React Cli
+```
+yarn global add react-native-cli
+```
+Install dependencies
+```
+cd ReactNativeDemo
+yarn install
+```
+To take build
+```
+react-native run-<platform>
+react-native start
+```
---
+++ index.ios.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  SectionList
+} from 'react-native';
+import data from './data';
+
+export default class sectionListExample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SectionList
+          renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.title}>{section.title}</Text>}
+          ItemSeparatorComponent={()=><View style={styles.itemSeparator}/>}
+          // keyExtractor={(item)=>item.name}
+          sections={data}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop:20,
+    backgroundColor: '#F5FCFF',
+  },
+  title:{
+    backgroundColor:'red',
+    color:'white',
+    padding : 10
+  },
+  item :{
+    padding:10
+  },
+  itemSeparator:{
+    height:1,
+    width:'100%',
+    backgroundColor:'#E8E8E8'
+  },
+});
+
+AppRegistry.registerComponent('sectionListExample', () => sectionListExample);
